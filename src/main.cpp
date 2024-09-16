d8e9f79c6e24481e09b11115bd1d8f2d184531ff
catb0239 <143758087+catb0239@users.noreply.github.com>
2024-09-16 05:22:29 +0000

    added functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,30 @@
 //******************************************************************************
 // File name:   main.cpp
-// Author:      CS Faculty
-// Date:        xx/xx/xxxx (Replace xx/xx/xxxx with month/day/year)
-// Class:       CS150-xx (Replace xx with your section number)
-// Assignment:  Hello World
-// Purpose:     Display the message Hello World to the screen
-// Hours:       0.25
+// Author:      Micah Catbagan
+// Date:        09/15/2024 (Replace xx/xx/xxxx with month/day/year)
+// Class:       CS200-01 (Replace xx with your section number)
+// Assignment:  Base Converter
+// Purpose:     Convert hex, decimal and binary
+// Hours:       too long.
 //******************************************************************************
 
 #include <iostream>
 
 using namespace std;
 
-int main () {
+int hexCharToInt (char hexDigit);
+char getBase(const string& strNumber);
+string getNumber (const string& prompt);
+void printTitle (const string& myTitle);
+string binaryToDecimal (const string& strNumber);
+string decimalToBinary (const string& strNumber);
+string decimalToHex (const string& strNumber);
+string hexToDecimal (const string& strNumber);
+string hexToBinary (const string& strNumber);
+string binaryToHex (const string& strNumber);
 
-  cout << "Hello World" << endl;
+
+int main () {
   
   return EXIT_SUCCESS;
 }
