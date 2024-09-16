33ee7563fdd252ec5ab72a4fd4ee9d5f3f4677c0
catb0239 <143758087+catb0239@users.noreply.github.com>
2024-09-16 18:30:46 +0000

    implementing hexCharToInt

--- src/main.cpp
+++ src/main.cpp
@@ -23,8 +23,42 @@ string hexToDecimal (const string& strNumber);
 string hexToBinary (const string& strNumber);
 string binaryToHex (const string& strNumber);
 
+/**************************************************************************
+ Function: 	 	main
 
+ Description: converting base
+
+ Parameters:	none
+
+ Returned:	 	Exit Status
+ *************************************************************************/
 int main () {
-  
+  char hexDigit;
+
+  cout << "Enter a hex digit: ";
+  cin >> hexDigit;
+
+  cout << "Your hex digit is " << hexCharToInt(hexDigit) << endl;
+
   return EXIT_SUCCESS;
+}
+
+/**************************************************************************
+ Function: 	 	hexCharToInt
+
+ Description: changes hex char to int
+
+ Parameters:	hexDigit - hex digit to be changed
+
+ Returned:	 	int to be changed to
+ *************************************************************************/
+int hexCharToInt (char hexDigit) {
+  const int ZERO = 0, TEN = 10;
+  int hexInt = ZERO, tempInt = hexDigit - '0';
+  if (tempInt > ZERO && tempInt < TEN) {
+    hexInt = tempInt;
+  } else if (hexDigit == 'A') {
+    hexInt = 10;
+  }
+  return hexInt;
 }
